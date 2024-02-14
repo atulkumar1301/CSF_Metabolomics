d66303d782afc2a8b6c29cda797b3cd29fc8c03d
ATUL KUMAR <atul.0298@gmail.com>
2024-02-14 13:25:10 +0100

    Update Volcano_Plot_Age_Adjusted_for_Sex.R

--- R_Code/Volcano_Plot_Age_Adjusted_for_Sex.R
+++ R_Code/Volcano_Plot_Age_Adjusted_for_Sex.R
@@ -8,8 +8,8 @@ library (plotly)
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 # The palette with black:
-cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
-#cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#CC79A7")
+cbbPalette <- c("#000000", "#D55E00", "#56B4E9", "#009E73", "#E69F00", "#0072B2", "#F0E442", "#CC79A7")
+
 
 df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_1/8_Full_Result_Data_Analysis_Age_Raw_Adjusted.txt")
 p <- ggplot (data = df, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
@@ -18,19 +18,18 @@ p <- p + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col
   geom_hline (aes (yintercept=-log10(0.044), linetype = "FDR p-value 0.05", col="#D55E00")) +
   scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
                         guide = guide_legend(override.aes = list(color = c("black", "#D55E00"))))
-#p <- p + geom_vline(xintercept=c(-0.165879033,  0.128549555), col="black")
 p <- p + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
-p <- p + scale_x_continuous(breaks = round(seq(-6.6, 11.8, by = 2),1))
+p <- p + scale_x_continuous(breaks = round(seq(-7, 12, by = 1),1))
 p <- p + scale_y_continuous(breaks = round (seq (0, 95, by = 5), 1))
 p <- p + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
 p <- p +
   theme(legend.position="left",
-               plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
-               axis.title.x = element_text(family = "serif", size=16),
-               axis.title.y = element_text(family = "serif", size=16),
-               axis.text.x = element_text(family = "serif", size=10, angle = 10),
-               axis.text.y = element_text(family = "serif", size=10),
-               legend.title = element_text(family = "serif", size=16),
-               legend.text = element_text(family = "serif", size=16),
-               panel.background = element_blank()) + labs(title="Differential Regulation of CSF Metabolites with respect to Age \n Adjusted for Sex")
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title="Differential Regulation of CSF Metabolites with respect to Age \n Adjusted for Sex")
 p
