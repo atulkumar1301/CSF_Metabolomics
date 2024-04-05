f7c48c5cf4cf9546c5d9ee9741f5bfae9cc2074e
ATUL KUMAR <atul.0298@gmail.com>
2024-04-05 11:42:44 +0200

    Create Volcano_Plot_Asyn.R

---
+++ R_Code/Volcano_Plot_Asyn.R
@@ -0,0 +1,35 @@
+#Volcano plot
+library(ggpubr)
+library(data.table)
+library(ggplot2)
+library(ggrepel)
+library (plotly)
+# The palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# The palette with black:
+cbbPalette <- c("#000000", "#D55E00", "#56B4E9", "#009E73", "#E69F00", "#0072B2", "#F0E442", "#CC79A7")
+
+
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/A_Syn/7_Full_Result_Data_Analysis_A_Syn.txt")
+p <- ggplot (data = df, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p <- p + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.01), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00"))))
+p <- p + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p <- p + scale_x_continuous(breaks = round(seq(-3, 2, by = 0.5), 1))
+p <- p + scale_y_continuous(breaks = round (seq (0, 8, by = 0.5), 1))
+p <- p + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
+p <- p +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("Differential Regulation of CSF Metabolites with respect to SAA Status"))
+p
