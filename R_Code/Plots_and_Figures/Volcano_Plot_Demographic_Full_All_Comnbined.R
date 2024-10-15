7e04324761c7d6df6b87840ee088be42122e6a5c
ATUL KUMAR <atul.0298@gmail.com>
2024-10-15 16:05:08 +0200

    Create Volcano_Plot_Demographic_Full_All_Comnbined.R

---
+++ R_Code/Plots_and_Figures/Volcano_Plot_Demographic_Full_All_Comnbined.R
@@ -0,0 +1,171 @@
+#Volcano plot
+library(ggpubr)
+library(data.table)
+library(ggplot2)
+library(ggrepel)
+library (plotly)
+
+
+# The palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# The palette with black:
+cbbPalette <- c("#000000", "#D55E00", "#56B4E9", "#009E73", "#E69F00", "#0072B2", "#F0E442", "#CC79A7")
+
+####Age
+
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Demographics/Full/7_Full_Result_Data_Analysis_Age_Full.txt")
+p <- ggplot (data = df, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p <- p + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.02087765957), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00518617021 (with No Dynamics)
+p <- p + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p <- p + scale_x_continuous(breaks = round(seq(-0.07, 0.04, by = 0.01),1))
+p <- p + scale_y_continuous(breaks = round (seq (0, 30, by = 2), 1))
+p <- p + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
+p <- p +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("a) Age"))
+p
+
+
+###Gender
+
+df_2 <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Demographics/Full/7_Full_Result_Data_Analysis_Gender_Full.txt")
+p_2 <- ggplot (data = df_2, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p_2 <- p_2 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.03271276595), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00119680851 (with No Dynamics)
+p_2 <- p_2 + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p_2 <- p_2 + scale_x_continuous(breaks = round(seq(-0.8, 0.6, by = 0.2),1))
+p_2 <- p_2 + scale_y_continuous(breaks = round (seq (0, 55, by = 4), 1))
+p_2 <- p_2 + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(p)))
+p_2 <- p_2 +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("b) Sex"))
+p_2
+
+####CU_CI
+
+df_4 <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Demographics/Full/7_Full_Result_Data_Analysis_A_Syn.txt")
+p_4 <- ggplot (data = df_4, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p_4 <- p_4 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.01276595744), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00864361702 (with No Dynamics)
+p_4 <- p_4 + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p_4 <- p_4 + scale_x_continuous(breaks = round(seq(-0.8, 0.5, by = 0.2), 1))
+p_4 <- p_4 + scale_y_continuous(breaks = round (seq (0, 15, by = 2), 1))
+p_4 <- p_4 + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(p)))
+p_4 <- p_4 +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("c) CU/CI Status"))
+p_4
+
+
+####Age adjusted for Gender and CU/CI
+
+df_1 <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Metabolites/All_Combined/7_Full_Result_Data_Analysis_Ab.txt")
+p_1 <- ggplot (data = df_1, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p_1 <- p_1 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.00319148936), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00039893617 (with no Dynamics)
+p_1 <- p_1 + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p_1 <- p_1 + scale_x_continuous(breaks = round(seq(-0.3, 0.3, by = 0.1),1))
+p_1 <- p_1 + scale_y_continuous(breaks = round (seq (0, 8, by = 1), 1))
+p_1 <- p_1 + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(P)))
+p_1 <- p_1 +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("a) Age adjusted for Sex and CU/CI status"))
+p_1
+
+
+###Gender adjusted for Age and CU/CI
+
+df_3 <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Metabolites/All_Combined/7_Full_Result_Data_Analysis_Taupet.txt")
+p_3 <- ggplot (data = df_3, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p_3 <- p_3 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.00026595744), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.00053191489 (with No Dynamics)
+p_3 <- p_3 + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p_3 <- p_3 + scale_x_continuous(breaks = round(seq(-0.6, 0.4, by = 0.2),1))
+p_3 <- p_3 + scale_y_continuous(breaks = round (seq (0, 8, by = 1), 1))
+p_3 <- p_3 + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(p)))
+p_3 <- p_3 +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("b) Sex adjusted for Age and CU/CI status"))
+p_3
+
+
+##CU/CI adjusted for Gender and Age
+
+df_5 <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Metabolites/All_Combined/7_Full_Result_Data_Analysis_Asyn.txt")
+p_5 <- ggplot (data = df_5, aes (x = Effect, y = -log10(P), col = Regulation, label = Label))+
+  geom_point ()+ geom_text_repel(max.overlaps = Inf, show.legend  = F)
+p_5 <- p_5 + geom_hline (aes(yintercept=-log10(0.05), linetype = "p-value 0.05", col="black")) +
+  geom_hline (aes (yintercept=-log10(0.00146276595), linetype = "FDR p-value 0.05", col="#D55E00")) +
+  scale_linetype_manual(name = "p-value cut off", values = c(2, 2), 
+                        guide = guide_legend(override.aes = list(color = c("black", "#D55E00")))) ##0.01316489361 (with No Dynamics)
+p_5 <- p_5 + scale_color_manual(values=cbbPalette, limits = force) + theme_light()
+p_5 <- p_5 + scale_x_continuous(breaks = round(seq(-0.3, 0.7, by = 0.3), 1))
+p_5 <- p_5 + scale_y_continuous(breaks = round (seq (0, 17, by = 1), 1))
+p_5 <- p_5 + xlab ("Effect") + labs (color = "Regulation") + ylab (expression (-log[10]~(p)))
+p_5 <- p_5 +
+  theme(legend.position="left",
+        plot.title = element_text(family = "serif", size=18, face = "bold", hjust = 0.5),
+        axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12),
+        axis.text.y = element_text(family = "serif", size=12),
+        legend.title = element_text(family = "serif", size=16),
+        legend.text = element_text(family = "serif", size=16),
+        panel.background = element_blank()) + labs(title=expression("c) CU/CI status adjusted for Age and Sex"))
+p_5
+
+
+plot <- ggarrange(p, p_2, p_4, p_1, p_3, p_5, common.legend = TRUE, legend = "bottom")
+annotate_figure(plot, top = text_grob(expression("Effects of demographics on CSF Metabolites"), color = "#999999", face = "bold", size = 20, family = "serif"))
