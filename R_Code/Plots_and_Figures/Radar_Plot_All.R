284db4b278b240e84a722227fc4c3a876fc7caec
ATUL KUMAR <atul.0298@gmail.com>
2024-10-21 16:59:00 +0200

    Create Radar_Plot_All.R

---
+++ R_Code/Plots_and_Figures/Radar_Plot_All.R
@@ -0,0 +1,29 @@
+# Library
+library(fmsb)
+library(data.table)
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Metabolites/Radar_Plot_Data_T_Value_Pathalogy_Adjusted.txt", check.names = FALSE)
+df <- data.frame(df, row.names = 1, check.names = FALSE)
+op <- par(family = "serif", font=1)
+create_beautiful_radarchart <- function(data, color = c("#009E73", "#E7B800", "#FC4E07", "#56B4E9"), 
+                                        vlabels = colnames(df), vlcex = 1,
+                                        caxislabels = NULL, title = NULL, ...){
+  radarchart(
+    df, axistype = 1, seg = 7,
+    # Customize the polygon
+    pcol = color, pfcol = scales::alpha(color, 0.3), plwd = 1, plty = 5,
+    # Customize the grid
+    cglcol = "black", cglty = 1, cglwd = 0.8,
+    # Customize the axis
+    axislabcol = "black", 
+    # Variable labels
+    vlcex = vlcex, vlabels = vlabels,
+    caxislabels = caxislabels, title = title, ...
+  )
+}
+create_beautiful_radarchart(df, caxislabels = c(-7, -5, -3, 1, 3, 5, 7))
+
+legend(
+  x = "bottom", legend = rownames(df[-c(1,2),]), horiz = TRUE,
+  bty = "n", pch = 20 , col = c("#009E73", "#E7B800", "#FC4E07", "#56B4E9"),
+  text.col = "black", cex = 1.5, pt.cex = 2
+)
