834718ba905f79b07a84ffc45acc2a487f8eb39f
ATUL KUMAR <atul.0298@gmail.com>
2024-11-06 15:09:49 +0100

    Update Bar_Plot.R

--- R_Code/Plots_and_Figures/Bar_Plot.R
+++ R_Code/Plots_and_Figures/Bar_Plot.R
@@ -2,11 +2,21 @@
 library(data.table)
 library (ggplot2)
 
-df <- fread ("Result_Mean_Metabolite_Level_Pathology.txt")
+df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Mean_Metabolite_Level_Pathology/Result_Mean_Metabolite_Level_Pathology.txt")
 
 p <- ggplot(data=df, aes(x=Model, y=`t Value`)) +
-  geom_bar(stat="identity")
+  geom_bar(stat="identity", width = 0.5) 
+  
 
-p + scale_x_discrete(limits=c(expression("A"*beta), expression("A"*beta*" adjusted for other co-pathology"), "TauPET", "TauPET adjusted for other co-pathology",
-                              expression (alpha*"Syn SAA"), expression (alpha*"Syn SAA adjusted for other co-pathology"), "WML", "TWML adjusted for other co-pathology"))
+p <- p + scale_x_discrete(breaks=c("Abeta","Abeta Adjusted","Tau PET", "Tau PET Adjusted", "SAA", "SAA Adjusted", "WML", "WML Adjusted"),
+                     labels=c(expression ("A"*beta), expression ("A"*beta*"*"), "TauPET", "TauPET*",
+                              expression (alpha*"Syn SAA"), expression(alpha*"Syn SAA*"), "WML", "WML*"))
+
+p <- p +
+  theme(axis.title.x = element_text(family = "serif", size=16),
+        axis.title.y = element_text(family = "serif", size=16),
+        axis.text.x = element_text(family = "serif", size=12, angle = 10),
+        axis.text.y = element_text(family = "serif", size=12)) +
+  labs(tag = "*adjusted for other co-pathology") +
+  theme(plot.tag.position = c(0.15, 0.02))
 p
