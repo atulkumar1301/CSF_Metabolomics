7f91ff2b3324b0432ef91d10ff4bd45eec2a4a51
ATUL KUMAR <atul.0298@gmail.com>
2025-11-05 16:32:12 +0100

    Update Bar_Plot.R

--- R_Code/Plots_and_Figures/Bar_Plot.R
+++ R_Code/Plots_and_Figures/Bar_Plot.R
@@ -2,21 +2,25 @@
 library(data.table)
 library (ggplot2)
 
-df <- fread ("/Volumes/ATUL_6TB/Work/Projects/CSF_Metabolomics/Analyses_2/Mean_Metabolite_Level_Pathology/Result_Mean_Metabolite_Level_Pathology.txt")
+df <- fread ("~/Library/CloudStorage/OneDrive-UniversityofEasternFinland/Work/Projects/CSF_Metabolomics/Analyses_2/Mean_Metabolite_Level_Pathology/Result_Mean_Metabolite_Level_Pathology.txt")
 
-p <- ggplot(data=df, aes(x=Model, y=`t Value`)) +
-  geom_bar(stat="identity", width = 0.5) 
-  
+p2 <- ggplot(data=df, aes(x=Model, y=`t Value`)) +
+  geom_bar(stat="identity", width = 0.5)
 
-p <- p + scale_x_discrete(breaks=c("Abeta","Abeta Adjusted","Tau PET", "Tau PET Adjusted", "SAA", "SAA Adjusted", "WML", "WML Adjusted"),
-                     labels=c(expression ("A"*beta), expression ("A"*beta*"*"), "TauPET", "TauPET*",
-                              expression (alpha*"Syn SAA"), expression(alpha*"Syn SAA*"), "WML", "WML*"))
+p2 <- p2 + theme_minimal()
+ 
 
-p <- p +
+p2 <- p2 + scale_x_discrete(breaks=c("Abeta","Abeta Adjusted","Tau PET", "Tau PET Adjusted", "SAA", "SAA Adjusted", "WML", "WML Adjusted",
+                                   "Age", "Age Adjusted", "Gender", "Gender Adjusted", "CU_CI", "CU_CI Adjusted"),
+                          labels=c(expression ("A"*beta), expression ("A"*beta*"*"), "TauPET", "TauPET*",
+                                   expression (alpha*"Syn SAA"), expression(alpha*"Syn SAA*"), "WML", "WML*", "Age", "Age*", "Gender", "Gender*", "CU/CI Status", "CU/CI status*"))
+p2 <- p2 + coord_flip()
+
+p2 <- p2 +
   theme(axis.title.x = element_text(family = "serif", size=16),
         axis.title.y = element_text(family = "serif", size=16),
-        axis.text.x = element_text(family = "serif", size=12, angle = 10),
-        axis.text.y = element_text(family = "serif", size=12)) +
+        axis.text.x = element_text(family = "serif", size=14),
+        axis.text.y = element_text(family = "serif", size=14)) +
   labs(tag = "*adjusted for other co-pathology") +
   theme(plot.tag.position = c(0.15, 0.02))
-p
+p2
